2698560e61927f9810c9a42a9a1088e78547e283
NahomyG <114265537+NahomyG@users.noreply.github.com>
2023-11-13 16:27:43 -0600

    commit 9

---
+++ guia2ejercicio6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guia2ejercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "Hola a todos, mi nombre es:";
+            string nombre = "Gabriela Nahomy Lopez";
+
+            foreach (char letra in texto )
+            {
+                Console.Write(" "+ letra + " " );
+            }
+
+            foreach (char palabras in nombre)
+            {
+                Console.Write( " " + palabras + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
